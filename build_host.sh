876a973f5b8cacf3c3f0839e043348bd7175e86a
Tobias Mädel <t.maedel@alfeld.de>
2021-12-01 12:46:52 +0100

    Add image building

--- build_host.sh
+++ build_host.sh
@@ -25,8 +25,10 @@ sed -i '/CheckSpace/d' /opt/mnt/etc/pacman.conf
 
 chroot /opt/mnt /build_arm.sh
 
+# create a 10GiB image file
 truncate -s 10G /opt/usb.img
 
+# partition it, MBR partition layout, 100MiB FAT32 config, rest as ext4
 sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /opt/usb.img
   o # clear the in memory partition table
   n # new partition
@@ -48,7 +50,39 @@ sed -e 's/\s*\([\+0-9a-zA-Z]*\).*/\1/' << EOF | fdisk /opt/usb.img
   q # and we're done
 EOF
 
+# loop mount the image file
 losetup -P /dev/loop0 /opt/usb.img
 
+# create both file systems
 mkfs.vfat /dev/loop0p1
 mkfs.ext4 /dev/loop0p2
+
+# create mount points
+mkdir /opt/usb_fat32
+mkdir /opt/usb_ext4
+
+# mount the filesystems
+mount /dev/loop0p1 /opt/usb_fat32
+mount /dev/loop0p2 /opt/usb_ext4
+
+# unmount the bind-mounts inside the chroot
+umount -fl /opt/mnt/proc
+umount -fl /opt/mnt/sys
+umount -fl /opt/mnt/dev
+
+umount /opt/mnt/proc
+umount /opt/mnt/sys
+umount /opt/mnt/dev
+
+# copy the root fs to the ext4 partition 
+rsync -a /opt/mnt/. /opt/usb_ext4/
+
+# unmount the partitions
+umount /opt/usb_fat32
+umount /opt/usb_ext4
+
+# unmount the loop mount
+losetup -D /dev/loop0
+
+# compress the resulting image (using multi-core gzip)
+time pigz /opt/usb.img
